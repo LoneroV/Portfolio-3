caffc9418dc0d508b251e6b7adc2895687388cb9
Vincent Lonero <vincent.lonero.16@gmail.com>
2015-01-12 12:09:06 -0800

    1/12 Commit
    Video 4 Completed

--- public_html/script.js
+++ public_html/script.js
@@ -2,20 +2,20 @@
 $("document").ready(function(){
     $(".shootout").css("color", "red");
     $("body").css("color", "goldenrod");
-    $("body").css("background-color", "grey");
+    $("body").css("background-image", "http://3.bp.blogspot.com/-p8VSjOLfm8Y/UErBemJHXrI/AAAAAAAAF0Y/opf3nKfKJss/s1600/Andrew-Luck-Colts.jpg");
     $("div:nth-child(2)").css("color", "blue");
     $("h1").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
     $("h1").bind("click", mouseClick);
 });
 
 function mouseOverMe(){
-    $("h1").html("VINCENT RULEZ!!!!1111!!1!!!");
+    $("h1").html("DemonOfLightning");
 }
 
 function mouseOutMe(){
-    $("h1").html(".......");
+    $("h1").html("Click me for more information");
 }
 
 function mouseClick(){
-    $("p").html("I am SOOOOOOOOOOOOOOOOOOO Tired LOLOLOLOLOLOMG");
+    $("p").html("");
 }
